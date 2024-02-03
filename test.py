3158fcecce9a1a6f31b8018cd03d0a6bcd7c1a2a
Kacper Kiszczomb <34841523+Kiszczomb@users.noreply.github.com>
2024-02-03 01:59:33 +0100

    Delete test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-TERMS = {"kgx" : "kgx", "gnome-terminal" : "gnome-terminal --zoom=1.2 --geometry=128x16 --", "xterm" : "xterm -r -e"}
-
-for term in TERMS:
-    cmd = ['which', term]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    o, e = proc.communicate()
-    if proc.returncode == 0:
-        print('command: ' + TERMS[term])
